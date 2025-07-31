e6f227d00f77bc4638e5b92bb7f63aa13af3ee40
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-07-31 18:22:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# task-tracker
+to-do list app
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {" "}
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <button className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+          Привет, Tailwind!
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
